60083632b117cdd4bcca0d7c21a13abc7edfdbec
Josprima Sihombing <jsihombing.id@gmail.com>
2024-07-02 00:48:03 +0700

    learn texture

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import "./style.css"
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 import GUI from "lil-gui";
+import gsap from "gsap";
 
 const sizes = {
   width: window.innerWidth,
@@ -39,6 +40,7 @@ customMesh.visible = false;
 const globalProperties = {
   color: "#ff0000",
   segment: 1,
+  wireframe: false,
 };
 
 const boxGeometry = new THREE.BoxGeometry(4, 4, 4, globalProperties.segment, globalProperties.segment, globalProperties.segment);
@@ -47,7 +49,9 @@ const box = new THREE.Mesh(boxGeometry, material);
 
 box.position.set(0, 2, 0);
 
-camera.position.set(0, 10, 20);
+camera.position.y = 4
+camera.position.z = 8
+
 controls.enableDamping = true;
 controls.update();
 
@@ -67,7 +71,6 @@ gui.addColor(globalProperties, "color").onChange((value: THREE.Color) => {
   box.material.color.set(value)
 })
 
-gui.add(material, "wireframe")
 gui.add(globalProperties, "segment")
   .min(1)
   .max(20)
@@ -81,11 +84,129 @@ helperGUI.add(axesHelper, "visible").name("axes helper")
 helperGUI.add(gridHelper, "visible").name("grid helper")
 
 scene.add(customMesh);
-scene.add(box);
-scene.add(gridHelper);
+// scene.add(box);
+// scene.add(gridHelper);
 scene.add(axesHelper);
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(devicePixelRatio, 2))
+
+/**
+ * Texture
+ */
+const textureLoader = new THREE.TextureLoader()
+const floorColorTexture = textureLoader.load("/materials/wood_01/basecolor.jpg");
+const floorAmbientOcclusionTexture = textureLoader.load("/materials/wood_01/ambientOcclusion.jpg");
+const floorNormalTexture = textureLoader.load("/materials/wood_01/normal.jpg");
+const floorRoughnessTexture = textureLoader.load("/materials/wood_01/roughness.jpg");
+
+floorColorTexture.colorSpace = THREE.SRGBColorSpace
+floorColorTexture.wrapS = THREE.RepeatWrapping
+floorColorTexture.wrapT = THREE.RepeatWrapping
+floorColorTexture.repeat.x=3
+floorColorTexture.repeat.y=3
+
+const groundColorTexture = textureLoader.load("/materials/ground/basecolor.png")
+const groundAmbientOcclusionTexture = textureLoader.load("/materials/ground/ambientOcclusion.png")
+const groundNormalTexture = textureLoader.load("/materials/ground/normal.png")
+const groundRoughnessTexture = textureLoader.load("/materials/ground/roughness.png")
+
+groundColorTexture.colorSpace = THREE.SRGBColorSpace
+groundColorTexture.wrapS = THREE.RepeatWrapping
+groundColorTexture.wrapT = THREE.RepeatWrapping
+groundColorTexture.repeat.x=3
+groundColorTexture.repeat.y=3
+
+/**
+ * Floor
+ */
+const floorGeometry = new THREE.BoxGeometry(3, 0.05, 3);
+const floorMaterial = new THREE.MeshStandardMaterial();
+const floor = new THREE.Mesh(floorGeometry, floorMaterial)
+
+floorMaterial.map = floorColorTexture
+floorMaterial.aoMap = floorAmbientOcclusionTexture
+floorMaterial.aoMapIntensity = 1
+floorMaterial.normalMap = floorNormalTexture
+floorMaterial.normalScale.set(0.5, 0.5)
+floorMaterial.roughnessMap = floorRoughnessTexture
+floorMaterial.roughness = 1
+
+/**
+ * Base
+ */
+const baseGeometry = new THREE.BoxGeometry(3.05, 0.4, 3.05)
+const baseMaterial = new THREE.MeshStandardMaterial()
+const base = new THREE.Mesh(baseGeometry, baseMaterial)
+
+base.position.y = -0.2-0.025
+base.position.z = -0.05/2
+base.position.x = -0.05/2
+
+baseMaterial.map = groundColorTexture
+baseMaterial.aoMap = groundAmbientOcclusionTexture
+baseMaterial.aoMapIntensity = 1
+baseMaterial.normalMap = groundNormalTexture
+baseMaterial.normalScale.set(0.5, 0.5)
+baseMaterial.roughnessMap = groundRoughnessTexture
+baseMaterial.roughness = 1
+
+/**
+ * Wall
+ */
+const wallLeftGeometry = new THREE.BoxGeometry(0.05, 2, 3.05);
+const wallBehindGeometry = new THREE.BoxGeometry(3, 2, 0.05);
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "#778979" })
+const wallLeft = new THREE.Mesh(wallLeftGeometry, wallMaterial)
+const wallBehind = new THREE.Mesh(wallBehindGeometry, wallMaterial)
+
+wallLeft.position.x = -1.5 - 0.05/2
+wallLeft.position.y = 1 - 0.05/2
+wallLeft.position.z = -0.05/2
+wallBehind.position.y = 1 - 0.05/2
+wallBehind.position.z = -1.5 - 0.05/2
+
+/**
+ * House group
+ */
+const houseGroup = new THREE.Group();
+
+houseGroup.add(base, wallLeft, wallBehind);
+houseGroup.rotation.y = -Math.PI / 4
+scene.add(houseGroup)
+
+/**
+ * Animation
+ */
+const timeline = gsap.timeline({ defaults: { ease: "expo" }, delay: 2 });
+
+timeline.from(base.position, { y: -4, duration: 0.4})
+timeline.from(base.scale, { x: 0, y: 0, duration: 0.4 }, "<0")
+timeline.from(floor.position, { y: 2, duration: 0.4})
+timeline.from(floor.scale, { x: 0, y:0, duration: 0.4 }, "<0")
+timeline.from(wallLeft.position, { x: -2, duration: 0.4})
+timeline.from(wallLeft.scale, { y: 0, x: 0, duration: 0.4}, "<0")
+timeline.from(wallBehind.position, { z: -2, duration: 0.4})
+timeline.from(wallBehind.scale, { y: 0, x : 0, duration: 0.4}, "<0")
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 30)
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
+pointLight.position.x = 0
+pointLight.position.y = 3
+pointLight.position.z = 0
+scene.add(pointLight)
+scene.add(pointLightHelper)
+
+gui.add(globalProperties, "wireframe").onChange((value: boolean) => {
+  floorMaterial.wireframe = value
+  wallMaterial.wireframe = value
+  baseMaterial.wireframe = value
+})
 
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
